24bad4a539e7de5ab426ac799f447dac1ebdcca5
Nate Krauss <nathanialkrauss@gmail.com>
2019-05-14 14:37:16 -0400

    still calibrating speed values

--- AutonomusRobotics_FollowOnTheTrack/AutonomusRobotics_FollowOnTheTrack.ino
+++ AutonomusRobotics_FollowOnTheTrack/AutonomusRobotics_FollowOnTheTrack.ino
@@ -110,34 +110,37 @@ void rightControl(int right_direction, int right_speed) { //function to simplify
 }
 
 void lineFollow() {
-  if (ignore < 1000 || readingAverage >= 17) { //when leader robot is not seen
+  if (ignore < 1000 || readingAverage >= 23) { //when leader robot is not seen
     if (data[0] < 40 && data[14] < 40) { //when line follower sensor only sees black
       rightControl(0,0);
       leftControl(0,0);
-    } else if (data[0] < 40 || data[2] < 40 || data[4] < 40 || data[6] < 40 || data[8] < 40 || data[10] < 40 || data[12] < 40) {
-      rightControl(0,200);
-      leftControl(0,50);
     } else if (data[14] < 40) {
-      rightControl(0,200);
-      leftControl(0,200);
+      rightControl(0,150);
+      leftControl(0,150);
+    } else if (data[0] < 40 || data[2] < 40 || data[4] < 40 || data[6] < 40 || data[8] < 40 || data[10] < 40 || data[12] < 40) {
+      rightControl(0,175);
+      leftControl(0,25);
     } else {
-      rightControl(0,50);
+      rightControl(0,25);
       leftControl(0,125);
     }
-  } else if (readingAverage < 17) { //when leader robot is being sensed to be close
+  } else if (10 < readingAverage < 23) { //when leader robot is being sensed 
     if (data[0] < 40 && data[14] < 40) { //when line follower sensor only sees black
       rightControl(0,0);
       leftControl(0,0);
-    } else if (data[0] < 40 || data[2] < 40 || data[4] < 40 || data[6] < 40 || data[8] < 40 || data[10] < 40 || data[12] < 40) {
-      rightControl(0,150); 
-      leftControl(0,50);
     } else if (data[14] < 40) {
-      rightControl(0,map(readingAverage, 4, 17, 0, 200));
-      leftControl(0,map(readingAverage, 4, 17, 0, 200));
+      rightControl(0,map(readingAverage, 10, 23, 0, 150));
+      leftControl(0,map(readingAverage, 10, 23, 0, 150));
+    } else if (data[0] < 40 || data[2] < 40 || data[4] < 40 || data[6] < 40 || data[8] < 40 || data[10] < 40 || data[12] < 40) {
+      rightControl(0,map(readingAverage, 10, 23, 0, 175)); 
+      leftControl(0,map(readingAverage, 10, 23, 0, 25));
     } else {
-      rightControl(0,50);
-      leftControl(0,125);
+      rightControl(0,map(readingAverage, 10, 23, 0, 25));
+      leftControl(0,map(readingAverage, 10, 23, 0, 125));
     }
+  } else if (readingAverage < 10) {
+    rightControl(0,0);
+    leftControl(0,0); 
   }
 }
 
